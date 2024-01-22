a34fa24f421077b6c93ef4bd4034d48fef37d8cb
musitive <dallinfrank@gmail.com>
2024-01-22 11:33:56 -0700

    Added functionality for newest version of Pro Tools and Unit Tests for that file

--- ProToolsMarkers.py
+++ ProToolsMarkers.py
@@ -12,6 +12,17 @@ import re
 class ProToolsMarkers:
     markers = []
 
+    def add_new_marker(self, line: str) -> None:
+        marker_data = re.split(r"\t", line)
+        marker_data = [x.strip() for x in marker_data]
+
+        if len(marker_data) == 6:
+            marker_id, location, time_reference, units, name, _ = marker_data
+        else:
+            marker_id, location, time_reference, units, name, _, _, _ = marker_data
+        self.markers.append(Marker(marker_id, location, time_reference, units, name, self.FRAME_RATE))
+
+
     def __init__(self, filename: str):
         timecode_file = open(filename, "r")
         timecode_index = 0
@@ -35,8 +46,7 @@ class ProToolsMarkers:
                 continue
 
             # Split the marker data and timestamp
-            marker_id, location, time_reference, units, name, _ = re.split("\s+", line)
-            self.markers.append(Marker(marker_id, location, time_reference, units, name, self.FRAME_RATE))
+            self.add_new_marker(line)
 
             # Update iterators
             timecode_index += 1
--- tests/TestProToolsMarkers.py
+++ tests/TestProToolsMarkers.py
@@ -9,20 +9,29 @@ from Marker import Marker
 
 class TestProToolsMarkers(unittest.TestCase):
     def setUp(self) -> None:
-        self.markers = ProToolsMarkers("test_01.txt")
+        self.markers_01 = ProToolsMarkers("test_01.txt")
+        self.markers_02 = ProToolsMarkers("test_02.txt")
 
     def test_init(self):
-        self.assertEqual(self.markers.FRAME_RATE, 23.976)
+        self.assertEqual(self.markers_01.FRAME_RATE, 23.976)
+        self.assertEqual(self.markers_02.FRAME_RATE, 23.976)
 
     def test_getMarker_fail(self):
-        self.assertRaises(Exception, lambda: self.markers.get_marker(-1))
+        self.assertRaises(Exception, lambda: self.markers_01.get_marker(-1))
 
-    def test_getMarker_pass(self):
+    def test_getMarker_pass_01(self):
         m = Marker("1", "01:00:01:02", "533760", "Samples", "1", 23.976)
-        self.assertEqual(self.markers.get_marker(0), m)
+        self.assertEqual(self.markers_01.get_marker(0), m)
 
         m = Marker("2", "01:00:15:10", "1222656", "Samples", "2", 23.976)
-        self.assertEqual(self.markers.get_marker(1), m)
+        self.assertEqual(self.markers_01.get_marker(1), m)
+
+    def test_getMarker_pass_02(self):
+        m = Marker("1", "01:00:21:09", "1171170", "Samples", "1", 23.976)
+        self.assertEqual(self.markers_02.get_marker(0), m)
+
+        m = Marker("2", "01:00:33:05", "1739738", "Samples", "2", 23.976)
+        self.assertEqual(self.markers_02.get_marker(1), m)
 
 if __name__ == '__main__':
     unittest.main()
