220b5785650c00c190e53d2272f9c000693bc66c
musitive <dallinfrank@gmail.com>
2024-01-23 11:36:28 -0700

    Refactored code into LDS Script Class

--- LdsScript.py
+++ LdsScript.py
@@ -1,7 +1,32 @@
 from docx import Document
+from docx.shared import Inches, Pt
+from docx.oxml.shared import OxmlElement,qn
+from docx.enum.section import WD_SECTION, WD_ORIENT
+from docx.oxml.ns import nsdecls
+from docx.oxml import parse_xml
+
+def color_cell(cell) -> None:
+    shading_elm_1 = parse_xml(r'<w:shd {} w:fill="F6CC9E"/>'.format(nsdecls('w')))
+    cell._tc.get_or_add_tcPr().append(shading_elm_1)
+
+def set_repeat_table_header(header_row):
+    """ set repeat table row on every new page
+    """
+    tr = header_row._tr
+    trPr = tr.get_or_add_trPr()
+    tblHeader = OxmlElement('w:tblHeader')
+    tblHeader.set(qn('w:val'), "true")
+    trPr.append(tblHeader)
+    return header_row
+
+def set_widths(row):
+    row.cells[0].width = Inches(0.5)
+    row.cells[1].width = Inches(1)
+    row.cells[2].width = Inches(1.5)
+    row.cells[3].width = Inches(3)
+    row.cells[4].width = Inches(3)
 
 class LdsScript:
-
     def __init__(self, wordFilename: str):
         self.document = Document(wordFilename)
 
@@ -19,3 +44,82 @@ class LdsScript:
 
     def get_translation(self, translationRow: int) -> str:
         return self.translation.cells[translationRow].text
+    
+    def change_orientation(self) -> None:
+        sections = self.document.sections
+
+        for section in sections:
+            # change orientation to landscape
+            section.orientation = WD_ORIENT.LANDSCAPE
+
+            new_width, new_height = section.page_height, section.page_width
+            section.page_width = new_width
+            section.page_height = new_height
+
+    def remove_table(self, index: int) -> None:
+        table = self.document.tables[index]
+        table._element.getparent().remove(table._element)
+
+    def remove_metadata(self) -> None:
+        self.remove_table(2)
+        self.remove_table(0)
+
+    def initialize_tables(self) -> None:
+        self.remove_metadata()
+
+        self.old_table = self.document.tables[0]
+        self.new_table = self.document.add_table(1, 5)
+        self.new_table.style = 'Table Grid'
+        set_repeat_table_header(self.new_table.rows[0])
+
+        self.initialize_table_headers()
+
+    def initialize_table_headers(self) -> None:
+        old_row = self.old_table.rows[0]
+        new_row = self.new_table.rows[0]
+
+        self.shift_translation(old_row, new_row, "IN", True)
+        for cell in new_row.cells:
+            color_cell(cell)
+    
+    def prevent_document_break(self) -> None:
+        """https://github.com/python-openxml/python-docx/issues/245#event-621236139
+        Globally prevent table cells from splitting across pages.
+        """
+        tags = self.document.element.xpath('//w:tr')
+        rows = len(tags)
+        for row in range(0, rows):
+            tag = tags[row]  # Specify which <w:r> tag you want
+            child = OxmlElement('w:cantSplit')  # Create arbitrary tag
+            tag.append(child)  # Append in the new tag
+
+    def add_row_to_new_table(self, translation_index: int, in_time: str) -> None:
+        old_row = self.old_table.rows[translation_index]
+        new_row = self.new_table.add_row()
+        self.shift_translation(old_row, new_row, in_time)
+
+    def shift_translation(self, old_row, new_row, in_time: str, bold: bool = False, style: str = 'Normal') -> None:
+        contents = old_row.cells[0].text
+        self.update_text(new_row, contents, 0, bold, style)
+        self.update_text(new_row, in_time, 1, bold, style)
+        
+        for n in range(1, len(old_row.cells)):
+            contents = old_row.cells[n].text
+            self.update_text(new_row, contents, n+1, bold, style)
+
+        set_widths(new_row)
+        return
+
+    def update_text(self, new_row, text: str, cell: int, bold: bool = False, style: str = 'Normal', ):
+        run = new_row.cells[cell].paragraphs[0].add_run(text)
+        new_row.cells[cell].paragraphs[0].style = self.document.styles[style]
+        run.bold = bold
+
+    def save_as_new_script(self, new_filename) -> None:
+        self.document.save(new_filename)
+
+    def set_styles(self) -> None:
+        style = self.document.styles['Normal']
+        font = style.font
+        font.name = 'Arial'
+        font.size = Pt(10)
--- timecode.py
+++ timecode.py
@@ -1,4 +1,5 @@
 from ProToolsMarkers import ProToolsMarkers
+from LdsScript import LdsScript
 
 from docx import Document
 from docx.shared import Inches, Pt
@@ -11,129 +12,39 @@ import shutil
 import re
 import codecs
 
-def changeOrientation(document) -> None:
-    sections = document.sections
-
-    for section in sections:
-        # change orientation to landscape
-        section.orientation = WD_ORIENT.LANDSCAPE
-
-        new_width, new_height = section.page_height, section.page_width
-        section.page_width = new_width
-        section.page_height = new_height
-
-    return
-
-def removeMetadata(table) -> None:
-    table._element.getparent().remove(table._element)
-    return
-
-def set_repeat_table_header(header_row):
-    """ set repeat table row on every new page
-    """
-    tr = header_row._tr
-    trPr = tr.get_or_add_trPr()
-    tblHeader = OxmlElement('w:tblHeader')
-    tblHeader.set(qn('w:val'), "true")
-    trPr.append(tblHeader)
-    return header_row
-
-def prevent_document_break(document):
-    """https://github.com/python-openxml/python-docx/issues/245#event-621236139
-       Globally prevent table cells from splitting across pages.
-    """
-    tags = document.element.xpath('//w:tr')
-    rows = len(tags)
-    for row in range(0, rows):
-        tag = tags[row]  # Specify which <w:r> tag you want
-        child = OxmlElement('w:cantSplit')  # Create arbitrary tag
-        tag.append(child)  # Append in the new tag
-
-def set_widths(row):
-    row.cells[0].width = Inches(0.5)
-    row.cells[1].width = Inches(1)
-    row.cells[2].width = Inches(1.5)
-    row.cells[3].width = Inches(3)
-    row.cells[4].width = Inches(3)
-
-def update_text(document, new_row, text: str, style: str, cell, bold: bool):
-    run = new_row.cells[cell].paragraphs[0].add_run(text)
-    new_row.cells[cell].paragraphs[0].style = document.styles[style]
-    run.bold = bold
-    # new_row.cells[cell].paragraphs[0].style.font.bold = bold
-
-def shiftTranslation(old_row, new_row, text: str, document, bold: bool, style: str = 'Normal') -> None:
-    update_text(document, new_row, old_row.cells[0].text, style, 0, bold)
-    update_text(document, new_row, text, style, 1, bold)
-    
-    for n in range(1, len(old_row.cells)):
-        update_text(document, new_row, old_row.cells[n].text, style, n+1, bold)
-
-    set_widths(new_row)
-    return
-
-def color_cell(cell):
-    shading_elm_1 = parse_xml(r'<w:shd {} w:fill="F6CC9E"/>'.format(nsdecls('w')))
-    cell._tc.get_or_add_tcPr().append(shading_elm_1)
-
 def enhance_script(filename: str, timecode_filename: str, new_filename: str) -> None:
-    document = Document(filename)
+    script = LdsScript(filename)
     markers = ProToolsMarkers(timecode_filename)
 
-    style = document.styles['Normal']
-    font = style.font
-    font.name = 'Arial'
-    font.size = Pt(11)
+    script.set_styles()
+    script.change_orientation()
+    script.initialize_tables()
 
-    changeOrientation(document)
-
-    removeMetadata(document.tables[2])
-    removeMetadata(document.tables[0])
-
-    old_table = document.tables[0]
-
-    new_table = document.add_table(1, 5)
-    new_table.style = 'Table Grid'
-    set_repeat_table_header(new_table.rows[0])
-
-    old_rows = old_table.rows
-    new_row = new_table.rows[0]
-
-    shiftTranslation(old_rows[0], new_row, "TIMECODE", document, True)
-    for cell in new_row.cells:
-        color_cell(cell)
     timecode_index = 0
+    translation_index = 1
 
-    def get_next_marker():
-        nonlocal timecode_index
+    n = len(markers.get_markers())
+
+    for timecode_index in range(n):
         marker = markers.get_marker(timecode_index)
-        name = marker.get_name()
-        timecode_index += 1
+        in_time = marker.get_timecode_in_frames()
 
-        while name == 'x' or name == 'w':
-            marker = markers.get_marker(timecode_index)
-            name = marker.get_name()
+        name = marker.get_name()
+        if name == 'x' or name == 'END':
             timecode_index += 1
             continue
 
-        return marker
+        script.add_row_to_new_table(translation_index, in_time)
+        translation_index += 1
 
-    for old_row in old_rows[1:]:
-        marker = get_next_marker()
-        
-        location = marker.get_timecode_in_frames()
 
-        new_row = new_table.add_row()
-        shiftTranslation(old_row, new_row, location, document, False)
+    script.prevent_document_break()
+    script.remove_table(0)
 
-    prevent_document_break(document)
-
-    removeMetadata(document.tables[0])
-
-    document.save(new_filename)
+    script.save_as_new_script(new_filename)
 
     return
 
 # Test Case
 if __name__ == '__main__':
-    enhance_script("test.docx", "BMVL_308_Timecode.txt", "output.docx")
+    enhance_script("tests/test.docx", "tests/BMVL_308_Timecode.txt", "tests/output.docx")
